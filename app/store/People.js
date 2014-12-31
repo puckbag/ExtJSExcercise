cddeca998dd1f9ccf859e5a7786f739060ab3128
Travis Miller <travis@travismiller.com>
2014-12-31 11:39:41 -0600

    grid refresh hack on store update

--- app/store/People.js
+++ app/store/People.js
@@ -1,7 +1,33 @@
+// TODO: find a better way to refresh the grid on store update.
+//       this is necessary because the grid grows outside the container
+//       if a rows height expands.
+
 Ext.define('ExtJSExercise.store.People', {
     extend: 'Ext.data.Store',
     model: 'ExtJSExercise.model.Person',
     autoLoad: true,
     autoSync: true,
-    pageSize: 10
+    pageSize: 10,
+
+    onCreateRecords: function(records, operation, success) {
+        var gridpanel = Ext.ComponentQuery.query('people-gridpanel');
+        if (gridpanel.length) {
+            gridpanel[0].getView().refresh();
+        }
+    },
+
+    onUpdateRecords: function(records, operation, success) {
+        var gridpanel = Ext.ComponentQuery.query('people-gridpanel');
+        if (gridpanel.length) {
+            gridpanel[0].getView().refresh();
+        }
+    },
+
+    onDestroyRecords: function(records, operation, success) {
+        var gridpanel = Ext.ComponentQuery.query('people-gridpanel');
+        if (gridpanel.length) {
+            gridpanel[0].getView().refresh();
+        }
+    }
+
 });
